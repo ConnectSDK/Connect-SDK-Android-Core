c63f06f634ae7fa96264ba724fc0cdec7a02bab7
Hyun Kook Khang <hyunkook.khang@lge.com>
2015-04-09 16:57:12 +0900

    Removed unused import

--- src/com/connectsdk/device/ConnectableDevice.java
+++ src/com/connectsdk/device/ConnectableDevice.java
@@ -22,7 +22,6 @@ package com.connectsdk.device;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
