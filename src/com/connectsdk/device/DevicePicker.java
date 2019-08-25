052547e4513807916f125aa3781569a35823377a
Lindsay-Needs-Sleep <lindsay-needs-sleep@outlook.com>
2019-08-25 17:06:23 -0600

    The device picker should actually notify the listener of cancel

--- src/com/connectsdk/device/DevicePicker.java
+++ src/com/connectsdk/device/DevicePicker.java
@@ -22,6 +22,7 @@ package com.connectsdk.device;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -77,7 +78,7 @@ public class DevicePicker {
      * @param message The title for the AlertDialog
      * @param listener The listener for the ListView to get the item that was clicked on
      */
-    public AlertDialog getPickerDialog(String message, final OnItemClickListener listener) {
+    public AlertDialog getPickerDialog(String message, final OnItemClickListener listener, DialogInterface.OnCancelListener cancelListener) {
         final DevicePickerListView view = new DevicePickerListView(activity);
 
         TextView title = (TextView) activity.getLayoutInflater().inflate(android.R.layout.simple_list_item_1, null);
@@ -85,7 +86,7 @@ public class DevicePicker {
 
         final AlertDialog pickerDialog = new AlertDialog.Builder(activity)
         .setCustomTitle(title)
-        .setCancelable(true)
+        .setOnCancelListener(cancelListener)
         .setView(view)
         .create();
 
